7d908dd90e58cd75791b7ff3d6c7c5a8d80521bb
Dominik Palacz <palacz.dominik@gmail.com>
2019-10-30 20:41:49 +0100

    First working

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -50,4 +50,23 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   createAudio(keyArray);
+
+  const playSound = e => {
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!sound) return;
+
+    key.classList.add("playing");
+    sound.currentTime = 0;
+    sound.play();
+  };
+
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
+  }
+
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+  window.addEventListener("keydown", playSound);
 });
