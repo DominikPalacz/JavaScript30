2cc3582f3ff6fd35b69fa002e44c4c5b65acc2bf
Dominik Palacz <palacz.dominik@gmail.com>
2019-11-03 11:32:05 +0100

    Done day 5

--- 00 - Title/js/app.js
+++ 00 - Title/js/app.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM ok :");
+
+  const fragment = document.createDocumentFragment();
+  const section = document.createElement("section");
+  section.innerHTML = `<div class="section"></div>`;
+  fragment.appendChild(section);
+  document.body.appendChild(fragment);
 });
