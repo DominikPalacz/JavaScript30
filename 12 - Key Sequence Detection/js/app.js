d918cc059dab0023e442c503b51671a493147e08
Dominik Palacz <palacz.dominik@gmail.com>
2019-11-11 19:29:27 +0100

    Added key sequencing detection

---
+++ 12 - Key Sequence Detection/js/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const fragment = document.createDocumentFragment();
+  const section = document.createElement("section");
+  section.classList.add("section-12");
+
+  section.innerHTML = `<div class="section">
+
+  <p>magic</p>
+  That's the basic of getting up a key sequencing detection... magic
+  <p>magic</p> magic
+  <h1>If you still don't know what to do</h1>
+  <h2>Select this text with the mouse and => <strong>Write the word... magic</strong></h2>
+  </div>`;
+  fragment.appendChild(section);
+  document.body.appendChild(fragment);
+
+  const pressed = [];
+  const password = "magic";
+
+  window.addEventListener("keyup", ({ key }) => {
+    console.log("e :", key);
+    pressed.push(key);
+    pressed.splice(-password.length - 1, pressed.length - password.length);
+    console.log(pressed);
+    if (pressed.join("").includes(password)) cornify_add();
+  });
+});
